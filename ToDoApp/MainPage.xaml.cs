566edc8d16d5f5ae1f5bd9d28276ce0d39e9f9c9
Andrei <andrei-eusebiu.dabelea@student.upt.ro>
2026-02-24 19:38:54 +0200

    Completed TaskEntryBorder (Last working build)

--- ToDoApp/MainPage.xaml.cs
+++ ToDoApp/MainPage.xaml.cs
@@ -12,5 +12,24 @@
             
         }
 
+        private void OnEntryAdded(object sender, EventArgs e)
+        {
+            // 1. Obține referința la câmpul Entry care a declanșat evenimentul
+            var entry = (Entry)sender;
+
+            // 2. Citește textul introdus de utilizator
+            string subtask = entry.Text;
+
+            // 3. Verifică dacă textul nu este gol sau format doar din spații
+            if (!string.IsNullOrWhiteSpace(subtask))
+            {
+                // 4. Aici vei adăuga logica de salvare a subtask-ului în listă
+                // TODO: add subtask to your list
+
+                // 5. Golește câmpul după confirmare, pregătit pentru next input
+                entry.Text = string.Empty;
+            }
+        }
+
     }
 }
